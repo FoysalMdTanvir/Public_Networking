edb5c3067ccb2e5fcecc9510acaf7d1c07dd9a51
MdTanvirFoysal <tanvirfaisal54@gmail.com>
2021-02-28 22:13:50 +0600

    Creating Models

--- App_Content/admin.py
+++ App_Content/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from App_Content.models import Category, Content, Comment, Likes
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Content)
+admin.site.register(Comment)
+admin.site.register(Likes)
--- App_Content/models.py
+++ App_Content/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Content(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    caption_content = models.TextField(verbose_name="What is on your mind?")
+    image = models.ImageField(upload_to='content_images', verbose_name="Image")
+    publish_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish_date', ]
+
+
+class Comment(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE, related_name='content_comment')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comment')
+    comment = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-comment_date',)
+
+    def __str__(self):
+        return self.comment
+
+
+class Likes(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE, related_name="liked_content")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liker_user")
+
+    def __str__(self):
+        return self.user + "likes" + self.content
