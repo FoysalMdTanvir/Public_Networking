a7c49a3a43f39f9b24e221658c7fcd5e62055b5c
MdTanvirFoysal <tanvirfaisal54@gmail.com>
2021-03-09 12:26:56 +0600

    User Sign Up & Login modified 2

--- App_Login/forms.py
+++ App_Login/forms.py
@@ -16,7 +16,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('email', 'username', 'password1', 'password2')
 
 
 class UserLogin(AuthenticationForm):
