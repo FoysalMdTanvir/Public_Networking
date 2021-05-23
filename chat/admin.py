3a4b44dff74624a76dcb274cb74500280ec69920
MdTanvirFoysal <tanvirfaisal54@gmail.com>
2021-05-23 12:01:25 +0600

    new chat delete

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from chat.models import Room, Message
 
 # Register your models here.
-admin.site.register(Room)
-admin.site.register(Message)
--- chat/urls.py
+++ chat/urls.py
@@ -4,9 +4,5 @@ from chat import views
 app_name = 'chat'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('<str:room>/', views.room, name='room'),
-    path('checkview', views.checkview, name='checkview'),
-    path('send', views.send, name='send'),
-    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+
 ]
